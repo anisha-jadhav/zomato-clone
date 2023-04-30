ed7283fd05a39f820ac080cf2e0174a96be8dcec
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-04-30 11:22:05 +0530

    Completed auth, food and restaurant routes.

---
+++ server/api/food/index.js
@@ -0,0 +1,73 @@
+import express from "express";
+
+import { FoodModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+/**
+ * Route    /:id
+ * Desc     get food based on id
+ * Params   _id
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const food = await FoodModel.findById(_id);
+    return res.json({ food });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * Route    /r/:id
+ * Desc     get all food based on particular restaurant
+ * Params   _id
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/r/:_id", async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const foods = await FoodModel.find({
+      restaurant: _id,
+    });
+    return res.json({ foods });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+/**
+ * Route    /c/:category
+ * Desc     get all food based on particular category
+ * Params   category
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/c/:category", async (req, res) => {
+  try {
+    const { category } = req.params;
+    const foods = await FoodModel.find({
+      category: { $regex: category, $options: "i" },
+    });
+      
+      if(!foods)    return res.status(404).json({error : `No food matched with ${category}`})
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// c:non
+// non ==== nonveg
+// non ===  nonassadg  using regex
+
+export default Router;
---
+++ server/api/restaurant/index.js
@@ -0,0 +1,90 @@
+import express from "express";
+
+import { RestaurantModel } from "../../database/allModels";
+
+const Router = express.Router();
+
+/**
+ * Route    /:city
+ * Desc     get all the restaurant based on city
+ * Params   none
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/", async (req,res) => {
+    try {
+        // http://localhost:4000/restaurant/?city=mumbai
+        const restaurants = await RestaurantModel.find({ city })
+        if (restaurants.length === 0) {
+            return res.json({error : 'No restaurant found in this city'})
+        }
+        return res.json({restaurants})
+
+    }
+    catch(error)
+    {
+        return res.status(500).json({error:error.message})
+
+    }
+})
+
+/**
+ * Route    /:_id
+ * Desc     get particular restaurant details based on id
+ * Params   _id
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/:_id", async (req,res) => {
+    try {
+        const { _id } = req.params
+        const restaurant = await RestaurantModel.findById(_id)
+        if (!restaurant) return res.status(404).json({ error: "Restaurant does not exits" })
+        
+        return res.json({restaurant})
+
+    }
+    catch(error)
+    {
+        return res.status(500).json({error:error.message})
+
+    }
+})
+
+/**
+ * Route    /search/:searchString
+ * Desc     get individual restaurant based on name as search string
+ * Params   searchString
+ * Access   public
+ * Method   Get
+ *
+ */
+
+Router.get("/search/:searchString", async (req,res) => {
+    try {
+        const { searchString } = req.params
+        const restaurants = await RestaurantModel.find({
+            name : {$regex: searchString, $options:'i'}
+        })
+
+        if (restaurants.length === 0) {
+            return res.status(404).json({error:"No such restaurant exist"})
+        }
+
+        return res.json({restaurants})
+
+
+    }
+    catch(error)
+    {
+        return res.status(500).json({error:error.message})
+
+    }
+})
+
+
+export default Router;
