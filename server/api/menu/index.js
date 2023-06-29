438aa471e0275db4083ce88ba2762d7375ee7a54
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-06-29 12:42:03 +0530

    Completed menu route.

--- server/api/menu/index.js
+++ server/api/menu/index.js
@@ -13,17 +13,24 @@ const Router = express.Router();
  * Method   GET
  *
  */
-/*
-Router.get("/:_id", async (req, res) => {
+
+Router.get("/list/:_id", async (req, res) => {
   try {
     const { _id } = req.params;
     const food = await MenuModel.findById(_id);
+
+    if (!food) {
+      return res
+        .status(404)
+        .json({ error: "No menu present for this restaurant" });
+    }
+
     return res.json({ food });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
-*/
+
 
 /**
  * Route    /images
@@ -37,11 +44,11 @@ Router.get("/:_id", async (req, res) => {
 Router.get("/images/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const getImages = await ImageModel.findOne({id});
+    const getImages = await ImageModel.findById(id);
     if (!getImages) {
       return res.status(404).json({ error: "No menu images found" });
     }
-    return res.json({ getImages });
+    return res.json({ menuImages : getImages });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
