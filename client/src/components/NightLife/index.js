197496aa2b1c8d94694bb3605f09756b30368815
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-07-08 23:51:39 +0530

    Completed nightlife and nutrition page.

--- client/src/components/NightLife/index.js
+++ client/src/components/NightLife/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+// components 
+import NightLifeCarousel from './NightCarousel';
+
 const NightLife = () => {
   return (
-    <div>NightLife</div>
-  )
+    <div className="mb-10">
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Nightlife Restaurant in Mumbai
+      </h1>
+      <NightLifeCarousel />
+    </div>
+  );
 }
 
 export default NightLife
