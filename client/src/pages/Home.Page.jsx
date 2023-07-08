6d722ee4bc4daa6c02d0943f2b873916398da908
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-07-08 20:34:41 +0530

    Completed restaurant card, food card, dining page.

--- client/src/pages/Home.Page.jsx
+++ client/src/pages/Home.Page.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 //layout
 import HomePageLayout from '../layouts/HomePage.layout'
+
+// components 
+import Delivery from '../components/Delivery'
+import Dining from "../components/Dining";
+import NightLife from "../components/NightLife";
+import Nutrition from "../components/Nutrition";
+
 const HomePage = () => {
+
+  const { type } = useParams()
+  
   return (
     <>
-    <h1>Home Page</h1></>
-  )
+      <div className="my-5 mb-20 md:mb-10">
+        {type === "delivery" && <Delivery/>}
+        {type === "dining" && <Dining/>}
+        {type === "night" && <NightLife/>}
+        {type === "nutri" && <Nutrition/>}
+      </div>
+    </>
+  );
 }
 
 export default HomePageLayout(HomePage)
