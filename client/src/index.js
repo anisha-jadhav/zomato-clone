b409769db15d1f4a771488db689e5e122820bc7d
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-07-19 00:14:23 +0530

    Integrated redux in frontend.

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from "axios";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+if (localStorage.zomatoUser) {
+  const { token } = JSON.parse(localStorage.zomatoUser);
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
   <React.StrictMode>
-     <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </React.StrictMode>
-  
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
