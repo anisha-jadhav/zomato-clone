8af6e98c8da380ccb4062c696d7c1835541c7cc9
anisha-jadhav <jadhavanisha3033@gmail.com>
2023-07-24 13:23:56 +0530

    Completed restaurant list and images.

--- client/src/pages/Home.Page.jsx
+++ client/src/pages/Home.Page.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 //layout
-import HomePageLayout from '../layouts/HomePage.layout'
+import HomePageLayout from "../layouts/HomePage.layout";
 
-// components 
-import Delivery from '../components/Delivery'
+// components
+import Delivery from "../components/Delivery";
 import Dining from "../components/Dining";
 import NightLife from "../components/NightLife";
 import Nutrition from "../components/Nutrition";
 
-const HomePage = () => {
+// redux
+import { useDispatch } from "react-redux";
+import { getRestaurant } from "../redux/reducers/restaurant/restaurant.action";
 
-  const { type } = useParams()
+const HomePage = () => {
+  const { type } = useParams();
   
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRestaurant());
+  }, []);
+
   return (
     <>
       <div className="my-5 mb-20 md:mb-10">
-        {type === "delivery" && <Delivery/>}
-        {type === "dining" && <Dining/>}
-        {type === "night" && <NightLife/>}
-        {type === "nutri" && <Nutrition/>}
+        {type === "delivery" && <Delivery />}
+        {type === "dining" && <Dining />}
+        {type === "night" && <NightLife />}
+        {type === "nutri" && <Nutrition />}
       </div>
     </>
   );
-}
+};
 
-export default HomePageLayout(HomePage)
+export default HomePageLayout(HomePage);
